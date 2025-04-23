a3a3eec388c8cd1d0d783c5a7ae64769491b7326
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-04-22 19:22:15 -0500

    complete until 3
    complete until reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //let string = "hello";
+    //if (string=="") return "ERROR";
+    let splitString = string.split("");
+    
+    let reversedString=""
+    let length = splitString.length;
+    for(i=0;i<length;i++){
+        reversedString += splitString.pop()
+    }
+    return reversedString;
 };
+
+//reverseString();
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//Psuedocode: 
+//1.take the input string
+//2. split it using the split operator, this will assign in an array
+//3. In a loop, take length-1 and join with length-2 and put it in a string
+//4. Output the reversed string in a different file 
