090220bb87d055565e85db4bdaae5fddf47eb9a0
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-04-23 13:19:45 -0500

    done with 04 and 05 of js excercise
    done with 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayList,...args) {
+
+    
+    newlist=arrayList;
+
+        for(arg of args)
+        {
+            newlist = newlist.filter(num => num!==arg)
+        }
+       
+       
+    return newlist 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//psuedocode
+//1. take the array and the element to remove from array as an argument
+//2. compare the array element with all the elements in array 
+//3. remove the matched element index from the array using pop 
+//4. return the leftover array or the manipulated array
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,41 @@
-const sumAll = function() {
+const sumAll = function( startAdd, finishAdd) {
+
+    sum=0;
+
+    // startAdd = Number(startAdd);
+    // finishAdd = Number(finishAdd);
+
+    // let largerNum = 0
+    // let smallerNum = 0
+
+    if (!Number.isInteger(startAdd) || !Number.isInteger(finishAdd) || startAdd<0 || finishAdd<0)
+        {
+            return "ERROR";
+        }
+    else if(startAdd>finishAdd) 
+        {
+        largerNum = startAdd;
+        smallerNum = finishAdd;
+        }
+    else
+        {
+        largerNum = finishAdd;
+        smallerNum = startAdd;
+        }
+
+    for(i=smallerNum;i<=largerNum;i++)
+        {
+            sum += i;
+        }
+
+    return sum;
 
 };
 
+//console.log(sumAll(1,4))
 // Do not edit below this line
 module.exports = sumAll;
+
+//1.take two integers as start and finish
+//2.loop from initialization of start number and finish until the finish number
+// provide the output
